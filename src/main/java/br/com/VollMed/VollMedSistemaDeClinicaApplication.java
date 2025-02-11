fc3b803abe2d369efd3478c7cc38873858a139fb
Thiago Jose <thiago.wilton2704@gmail.com>
2025-02-11 10:47:37 -0300

    chore: criar projeto base com Spring Boot
    - Estrutura inicial do projeto configurada com Spring Initializr.
    
    - Pom.xml contendo dependências do MySql e Flyway para configuração do banco de dados e controle de migrações.

---
+++ src/main/java/br/com/VollMed/VollMedSistemaDeClinicaApplication.java
@@ -0,0 +1,13 @@
+package br.com.VollMed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VollMedSistemaDeClinicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VollMedSistemaDeClinicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/VollMed/VollMedSistemaDeClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.VollMed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VollMedSistemaDeClinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
