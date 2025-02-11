2fb1dc97ecdf7b951fca4116db8b7f4043fc6dd9
Thiago Jose <thiago.wilton2704@gmail.com>
2025-02-11 12:07:28 -0300

    feat(model): Adicionando classe Doctor(entidade mapeada para tabela doctors).
    - Utilizadas anotações do Lombok para redução de código repetitivo (getters, setters, construtores).
    - Implementada associação com a entidade Address (endereço do médico).
    - Implementada associação com o tipo de especialidade (Speciality) com enum.

---
+++ src/main/java/br/com/VollMed/domain/doctor/Doctor.java
@@ -0,0 +1,37 @@
+package br.com.VollMed.domain.doctor;
+
+import br.com.VollMed.domain.Address.Address;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "doctors")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = {"id"})
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String cpf;
+
+    private String phone;
+
+    private LocalDateTime created_at;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private Speciality speciality;
+}
