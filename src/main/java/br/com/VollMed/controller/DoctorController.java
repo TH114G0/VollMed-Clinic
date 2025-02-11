40a4f70683918db87291858255132c7fa534bf6a
Thiago Jose <thiago.wilton2704@gmail.com>
2025-02-11 15:47:34 -0300

    feat: Criando construtor e adicionando funcionalidades.
    - Criando EndPoint para realizar a criação de um doutor.
    - Criando EndPoint para realizar a chamada de uma Lista(page ordenada).

---
+++ src/main/java/br/com/VollMed/controller/DoctorController.java
@@ -0,0 +1,36 @@
+package br.com.VollMed.controller;
+
+import br.com.VollMed.domain.doctor.Doctor;
+import br.com.VollMed.dto.DoctorCreateDTO;
+import br.com.VollMed.dto.DoctorListDTO;
+import br.com.VollMed.infrastructure.service.DoctorService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/doctors")
+public class DoctorController {
+
+    @Autowired
+    private DoctorService doctorService;
+
+    @PostMapping
+    public ResponseEntity<Doctor> create(@RequestBody @Valid DoctorCreateDTO doctorCreateDTO) {
+        var doctor = doctorService.create(doctorCreateDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(doctor);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<DoctorListDTO>> list(@PageableDefault(size = 10, sort = {"name"}) Pageable pageable) {
+        var doctor = doctorService.list(pageable);
+        return ResponseEntity.ok(doctor);
+    }
+
+
+}
